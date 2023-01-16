d00dedb701c0d96819ccd86963bf62049cbbec77
Michael Testut <michael.testut@yahoo.com>
2023-01-15 23:01:09 -0800

    initial commit

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Flash Chat iOS13
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
