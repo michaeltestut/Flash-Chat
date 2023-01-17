789e843ea3c5b96871f8dfd0545340c1b3535952
Michael Testut <michael.testut@yahoo.com>
2023-01-16 23:35:22 -0800

    added title animation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -11,7 +11,15 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        var charIndex=0.0
+        let titleText="⚡️FlashChat"
+        for letter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex+=1	
+        }
        
     }
     
