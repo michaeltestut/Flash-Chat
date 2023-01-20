358433b700ed4825de0d30279b97e78debc0b3c7
Michael Testut <michael.testut@yahoo.com>
2023-01-19 22:09:52 -0800

    logout added

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -5,6 +5,7 @@
 
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
@@ -13,11 +14,21 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title="⚡️FlashChat"
+        navigationItem.hidesBackButton=true
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,14 +14,6 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
         titleLabel.text="⚡️FlashChat"
-//        var charIndex=0.0
-//        let titleText="⚡️FlashChat"
-//        for letter in titleText{
-//            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { timer in
-//                self.titleLabel.text?.append(letter)
-//            }
-//            charIndex+=1
-//        }
        
     }
     
