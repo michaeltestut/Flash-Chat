2cb3a92be7d0e99e882ba8f3bfacb0b437ec4b48
Michael Testut <michael.testut@yahoo.com>
2023-01-19 21:09:42 -0800

    added login/registration

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -4,13 +4,26 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    @IBOutlet weak var errorLabel: UILabel!
+    
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email=emailTextfield.text, let password=passwordTextfield.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    self.errorLabel.text=e.localizedDescription
+                }
+                else{
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
