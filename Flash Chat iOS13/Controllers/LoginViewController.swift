2a4451a9e7140c2e24141f001393e7e472ab4835
Michael Testut <michael.testut@yahoo.com>
2023-01-19 21:51:56 -0800

    added login error handling messagges

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -5,6 +5,7 @@
 
 
 import UIKit
+import FirebaseCore
 import FirebaseAuth
 
 class LoginViewController: UIViewController {
@@ -12,12 +13,22 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
+    @IBOutlet weak var errorLabel: UILabel!
+    
     @IBAction func loginPressed(_ sender: UIButton) {
         if let email = emailTextfield.text, let password=passwordTextfield.text{
             Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
                 if let e=error{
-                    print(e)
+                    switch e.localizedDescription{
+                    case "The password is invalid or the user does not have a password.":
+                        self.errorLabel.text="invalid password"
+                    case "There is no user record corresponding to this identifier. The user may have been deleted.":
+                        self.errorLabel.text="There is no account associated with this email"
+                    case "The email address is badly formatted.":
+                        self.errorLabel.text="Must provide proper email address"
+                    default:
+                        self.errorLabel.text="Login error, please try again"
+                    }
                 }
                 else{
                     self.performSegue(withIdentifier: "LoginToChat", sender: self)
