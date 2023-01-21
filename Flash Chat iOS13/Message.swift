96531addfcb767c071ceaca6436c6e64f3edfff7
Michael Testut <michael.testut@yahoo.com>
2023-01-20 23:43:23 -0800

    messaging functionality started

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Michael Testut on 1/20/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender:String
+    let body:String
+}
